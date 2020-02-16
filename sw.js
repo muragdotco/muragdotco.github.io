1530e9ac3dc0aa1dc859496ad7a5e268da84e83f
Engkus Kusnadi <ngkusnd@gmail.com>
2020-02-16 15:04:10 +0700

    test using grow.io

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-importScripts('https://cdn.ampproject.org/sw/amp-sw.js');
+importScripts("https://cdn.ampproject.org/sw/amp-sw.js");
 
 AMP_SW.init({
-  assetCachingOptions: [{
-    regexp: /\.(png|jpg|woff2|woff|css|js)/,
-    cachingStrategy: 'CACHE_FIRST',
-  }],
+  assetCachingOptions: [
+    {
+      regexp: /\.(png|jpg|woff2|woff|css|js)/,
+      cachingStrategy: "CACHE_FIRST"
+    }
+  ],
   offlinePageOptions: {
-    url: '/offline.html',
-    assets: [],
-  },
+    url: "{{g.url('/root/offline.html').path}}",
+    assets: []
+  }
 });
